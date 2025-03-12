d5d339ed369318629b64b3129d8082767da7976a
Daniel Hughes <dannyboy1717@hotmail.co.uk>
2025-03-12 22:17:51 +0000

    Initial commit of MAUI project

---
+++ VendingChaser/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VendingChaser;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
